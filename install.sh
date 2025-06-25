f09cf3035dbcc28b632c43aed0afb14ef430fd62
Robinvpn <log1312@yahoo.com>
2025-06-26 00:43:10 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+.#!/bin/bash
+set -e
+
+curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
+chmod +x wireguard-install.sh
+echo -e "\n\n\nrobin\n51820\n1.1.1.1\n" | sudo ./wireguard-install.sh
